301b67f58e978fa889f7a203d8e29118928ff0a4
Cristiane Alves <carodsousa@yahoo.com.br>
2026-06-23 15:16:04 -0300

    Adiciona video do guia do cuidador

--- src/config/onboardingVideos.ts
+++ src/config/onboardingVideos.ts
@@ -1,6 +1,6 @@
 export type OnboardingVideoRole = "caregiver" | "family";
 
 export const onboardingVideoLinks: Record<OnboardingVideoRole, string> = {
-  caregiver: "",
+  caregiver: "https://youtu.be/o28PzafEWgQ",
   family: "",
 };
